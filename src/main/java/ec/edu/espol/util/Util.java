4d26907fa45f711c22ac9ebc5c4da2476db2a872
Michaelcede <74023391+Michaelcede@users.noreply.github.com>
2021-07-10 17:07:51 -0500

    Update Util.java
    Añadimos la conversion de contraseñas a Hash

--- src/main/java/ec/edu/espol/util/Util.java
+++ src/main/java/ec/edu/espol/util/Util.java
@@ -8,6 +8,11 @@ package ec.edu.espol.util;
 import ec.edu.espol.model.Vendedor;
 import java.io.File;
 import java.util.Scanner;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 /**
  *
@@ -15,7 +20,7 @@ import java.util.Scanner;
  */
 public class Util {
     
-    // Verifica si el vendedor esta en el sistema. Le das un archivo de texto con los vendedores
+    // Verifica si el vendedor esta en el sistema. Le das un onjeto Vendedor y un archivo de texto con los vendedores
     // Te bota un true sino se encuentra y false si, si lo esta.
     public boolean verificarVendedor(Vendedor v,String nomfile){
         try(Scanner sc = new Scanner (new File(nomfile))){
@@ -33,5 +38,23 @@ public class Util {
         return true;
     }
     
+    
+    //Para convertir contraseñas en tipo de dato Hash
+    //Desde aqui
+    public static byte[] getSHA(String input) throws NoSuchAlgorithmException
+    { 
+        MessageDigest md = MessageDigest.getInstance("SHA-256"); 
+        return md.digest(input.getBytes(StandardCharsets.UTF_8)); 
+    }
+    
+    public static String toHexString(byte[] hash){
+        BigInteger number = new BigInteger(1, hash); 
+        StringBuilder hexString = new StringBuilder(number.toString(16)); 
+        while (hexString.length() < 32) 
+        { 
+            hexString.insert(0, '0'); 
+        } 
+        return hexString.toString(); 
+    }
     
 }
