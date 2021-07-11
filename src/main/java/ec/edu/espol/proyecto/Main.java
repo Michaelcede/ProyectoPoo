5969cfdf3a682f8fed0ea07f02ab54d71c705fcb
RobertAlec <77252823+RobertAlec@users.noreply.github.com>
2021-07-10 23:11:42 -0500

    Update Main.java

--- src/main/java/ec/edu/espol/proyecto/Main.java
+++ src/main/java/ec/edu/espol/proyecto/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ec.edu.espol.proyecto;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +16,64 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Menu de opciones principal
+       
+        Scanner leer = new Scanner(System.in);
+        
+        int n;
+        boolean salir = false;
+        
+        while(!salir){
+            System.out.println("Menú de Opciones");
+            System.out.println("");
+            System.out.println("1. Vendedor");
+            System.out.println("2. Comprador");
+            System.out.println("3. Salir");
+
+            try(){
+                System.out.print("Elije una opcion del menú: ");
+                n = leer.nextInt(); //n recoge el valor escrito por teclado (opcion del menu)
+
+                switch(n){
+                    case 1:
+                        System.out.println("Has escogido la opción Vendedor");
+                        System.out.println("");
+                        int op1 = 0;
+                        do{
+                        System.out.println("1. Registrar un nuevo vendedor");
+                        System.out.println("2. Ingresar un nuevo vehículo");
+                        System.out.println("3. Aceptar oferta");
+                        System.out.println("4. Regresar");
+                        } while(op1 != 4);
+                        break;
+
+                    case 2:
+                        System.out.println("Has escogido la opción Comprador");
+                        System.out.println("");
+                        int op2 = 0;
+                        do{
+                        System.out.println("1. Registrar un nuevo vendedor");
+                        System.out.println("2. Ingresar un nuevo vehículo");
+                        System.out.println("3. Aceptar oferta");
+                        System.out.println("4. Regresar");
+                        } while(op2 != 4);
+                        break; 
+                    case 3:
+                        salir = true;
+                        System.out.println("kuchau");
+                        break;
+                    default:
+                         System.out.println("No has escogido una opcion del menú");
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Debes ingresar un número");
+                leer.next();
+            }
+        }
+        
+        System.out.println("Fin del meun");
         // TODO code application logic here
+        
     }
     
 }
