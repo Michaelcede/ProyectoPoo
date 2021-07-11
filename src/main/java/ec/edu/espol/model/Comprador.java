ede7c236ce93fb69e554c832ecfe7dcd217f4c93
RobertAlec <77252823+RobertAlec@users.noreply.github.com>
2021-07-10 20:41:35 -0500

    Update Comprador.java

--- src/main/java/ec/edu/espol/model/Comprador.java
+++ src/main/java/ec/edu/espol/model/Comprador.java
@@ -10,5 +10,68 @@ package ec.edu.espol.model;
  * @author micha
  */
 public class Comprador {
+    private String nombres;
+    private String apellidos;
+    private String correo_electronico;
+    private String organizacion;
+    private String correo;
+    private String clase;
+
+    public Comprador(String nombres, String apellidos, String correo_electronico, String organizacion, String correo, String clase) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo_electronico = correo_electronico;
+        this.organizacion = organizacion;
+        this.correo = correo;
+        this.clase = clase;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
+    public String getOrganizacion() {
+        return organizacion;
+    }
+
+    public void setOrganizacion(String organizacion) {
+        this.organizacion = organizacion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
     
 }
